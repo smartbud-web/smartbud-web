554b081d19d67f0b141109ea3d3bba9a127bf2fd
smartbud-web <smartbud.hk@gmail.com>
2026-07-05 16:59:36 +0800

    Create tailwind.config.js

---
+++ js/tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    50: '#FDFBF7',  // Soft luxurious warm cream background
+                    100: '#F6EFE0', // Richer sand tone
+                    200: '#E9DEC3', // Soft organic outline clay
+                    500: '#5F6F52', // Soft Sprout Green
+                    600: '#435334', // Resilient Pine Green
+                    700: '#254336', // Deep Forest Moss Green
+                    800: '#D2A350', // Warm Sunlight Yellow / Radiant Gold
+                    900: '#9CA777', // Tender Sprout Spurt Green
+                    dark: '#1C211A' // Elegant charcoal
+                }
+            },
+            fontFamily: {
+                sans: ['Nunito', 'Noto Sans TC', 'sans-serif'],
+                serif: ['Playfair Display', 'serif']
+            }
+        }
+    }
+}
